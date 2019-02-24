a5e23ea557e583da0ba8ca208a0e2d38b8fe26ea
hbquan93 <quanhuynh2007@gmail.com>
2019-02-24 22:41:55 +0700

    240219: class MyIntArray: Problem when don't setup method constructor copy

---
+++ c3ObjectAndMethod/problemWhenNotSetupConstructorCopy.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class MyIntArray
+{
+private:
+    int* pArr;
+    int size;
+public:
+    MyIntArray(int); // allocate, init pArr with value = 0
+    void update(int, int);
+    int get(int);
+};
+MyIntArray::MyIntArray(int sz)
+{
+    size = sz;
+    pArr = new int[size];
+    for (int i = 0; i < size; i++)
+        pArr[i] = 0;
+}
+void MyIntArray::update(int k, int val)
+{
+    pArr[k] = val;
+}
+int MyIntArray::get(int k)
+{
+    return pArr[k];
+}
+int main()
+{
+    cout << endl << "problemWhenNotSetupConstructorCopy.cpp" << endl;
+    MyIntArray a(10);
+    MyIntArray b(a);
+
+    cout << b.get(3) << endl;
+    cout << a.get(3) << endl;
+    a.update(3, 10); // a[3] = 10, effect b[3]
+    cout << b.get(3) << endl;
+    cout << a.get(3) << endl;
+    return 0;
+}
