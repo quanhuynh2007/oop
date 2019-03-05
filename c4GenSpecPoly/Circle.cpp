9cc1d90b5b3d0e439e6c159a5c20c96abfee7e5c
hbquan93 <quanhuynh2007@gmail.com>
2019-03-06 00:28:30 +0700

    060319 - branch master - remove file redundant

--- c4GenSpecPoly/Circle.cpp
+++
@@ -1,8 +0,0 @@
-#include "Circle.h"
-Circle::Circle(float r): Ellipse(r, r) {}
-
-void Circle::Input(istream& inDev)
-{
-    inDev >> Ra;
-    Rb = Ra;
-}
--- c4GenSpecPoly/Figure.cpp
+++
@@ -1,9 +0,0 @@
-#include "Figure.h"
-Figure::Figure(){}
-
-void Figure::Input(istream& inDev){}
-
-float Figure::Area()
-{
-    return 0;
-}
